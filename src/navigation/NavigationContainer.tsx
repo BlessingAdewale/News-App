d30bfe37d2f47b0ae45a7c8e529f4ec179db16a9
BlessingAdewale <adelekeblessingadewale@gmail.com>
2022-12-31 08:11:16 +0100

    First commit

---
+++ src/navigation/NavigationContainer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { NavigationContainer as BaseNavigationContainer } from '@react-navigation/native';
+
+
+export const NavigationContainer = () => {
+  return (
+    <BaseNavigationContainer>
+      
+    </BaseNavigationContainer>
+  );
+};
