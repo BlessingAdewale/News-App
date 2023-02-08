17a037bb157cbf24c4cca91371d3f11e61bff21c
BlessingAdewale <adelekeblessingadewale@gmail.com>
2023-02-08 13:54:17 +0100

    67% Completed

--- src/screens/Location/index.tsx
+++ src/screens/Location/index.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import { Button } from 'react-native-elements';
 
 export function Location() {
   return (
-    <View>
-      <Text>This is Location</Text>
+    <View style={styles.container}>
+      <Button title="Get Location" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#0244be',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    marginTop: 10,
+    color: 'white',
+  },
+});
